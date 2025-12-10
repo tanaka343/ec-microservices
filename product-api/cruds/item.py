62e9cb252e0f35773e3f305c37868a74e0c33915
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-12-10 15:23:24 +0900

    fix : update関数記載ミス修正

--- product-api/cruds/item.py
+++ product-api/cruds/item.py
@@ -32,8 +32,8 @@ def update(id :int,update_item :ItemUpdate,db :Session):
     
     item.name =item.name if update_item.name is None else update_item.name
     item.price =item.price if update_item.price is None else update_item.price
-    item.email =item.detail if update_item.detail is None else update_item.detail
-    item.email =item.category_id if update_item.category_id is None else update_item.category_id
+    item.detail =item.detail if update_item.detail is None else update_item.detail
+    item.category_id =item.category_id if update_item.category_id is None else update_item.category_id
     
     db.add(item)
     db.commit()
