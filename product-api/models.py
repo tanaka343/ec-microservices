c2334dc117c12f166c563d6792507ffb692dd2f3
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-12-10 14:51:33 +0900

    feat : categoriesテーブルモデルの作成と外部キーの追加
    - modelsにcategoriesテーブルデータモデル作成
    - modelsのproductsテーブルに外部キー追加
    - マイグレーションファイルの作成

--- product-api/models.py
+++ product-api/models.py
@@ -9,6 +9,11 @@ class Item(Base):
     name = Column(String,nullable=False)
     price = Column(String,nullable=False)
     detail = Column(String,nullable=True)
+    category_id = Column(Integer,ForeignKey("categories.id",name="fk_ctegory_id",ondelete="CASCADE"),nullable=False)
+    category = relationship("Category",back_populates="products")
 
-    
-
+class Category(Base):
+    __tablename__ = "categories"
+    id = Column(Integer,primary_key=True)
+    category_name = Column(String,nullable=False)
+    products = relationship("Item",back_populates="category")
