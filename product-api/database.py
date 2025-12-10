06b6ad7cb98335e8462eb15554bd088efb581661
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-12-10 09:26:16 +0900

    fix : alembicファイル作成エラー解決

--- product-api/database.py
+++ product-api/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
 
-SQLALCHEMY_URL = "sqlite:///../database.db"
+SQLALCHEMY_URL = "sqlite:///../product.db"
 
 engine = create_engine(SQLALCHEMY_URL)
 
