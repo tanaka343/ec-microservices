a774815288c1dbe11c9cb16614d99b6d564e43ed
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-12-12 15:17:40 +0900

    feat : product-api,stock-apiのポート番号を指定

--- product-api/main.py
+++ product-api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI,Request
 from starlette import status
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
-
+import uvicorn
 from routers import item,category
 
 app = FastAPI()
@@ -14,3 +14,7 @@ app.include_router(category.router)
 async def handler(request:Request, exc:RequestValidationError):
     print(exc)
     return JSONResponse(content={}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+
+if __name__ == "__main__":
+  uvicorn.run(app,host="127.0.0.1",port=8001)
--- stock-api/main.py
+++ stock-api/main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI,Request
 from starlette import status
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
-
+import uvicorn
 from routers import stock
 
 app = FastAPI()
 app.include_router(stock.router)
+
+if __name__ == "__main__":
+  uvicorn.run(app,host="127.0.0.1",port=8002)
