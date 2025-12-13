f1ea46a3b5b3d0dd2ec2c8b3b8ea3a36951f8f62
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-12-13 19:05:54 +0900

    feat : 例外処理とstatus_code=status.HTTP_200_OKを追加

--- product-api/cruds/ctegory.py
+++ product-api/cruds/ctegory.py
@@ -7,27 +7,34 @@ def find_all(db: Session):
 
 
 def find_by_id(id: int,db :Session):
-  return db.query(Category).filter(Category.id==id).first()
+  found_item = db.query(Category).filter(Category.id==id).first()
+  if not found_item:
+    return None
+  return found_item
 
 
 def create(db :Session,create_category :CategoryCreate):
-  new_category = Category(
+  new_item = Category(
     **create_category.model_dump()
   )
-  db.add(new_category)
+  db.add(new_item)
   db.commit()
-  return new_category
+  return new_item
 
 def update(id :int,db :Session,update_category :CategoryUpdate):
-  item = find_by_id(id,db)
-  item.category_name =item.category_name if update_category.category_name is None else update_category.category_name
-  db.add(item)
+  update_item = find_by_id(id,db)
+  if not update_item:
+    return None
+  update_item.category_name =update_item.category_name if update_category.category_name is None else update_category.category_name
+  db.add(update_item)
   db.commit()
-  return item
+  return update_item
 
 
 def delete(id :int,db :Session):
-  delete_category = find_by_id(id,db)
-  db.delete(delete_category)
+  delete_item = find_by_id(id,db)
+  if not delete_item:
+    return None
+  db.delete(delete_item)
   db.commit()
-  return delete_category
+  return delete_item
--- product-api/routers/category.py
+++ product-api/routers/category.py
@@ -1,30 +1,39 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter,Depends,HTTPException
 from database import get_db
 from sqlalchemy.orm import Session
 from typing import Annotated
 from cruds import ctegory as category_cruds
 from schemas import CategoryResponse,CategoryCreate,CategoryUpdate
+from starlette import status
 
 router = APIRouter(prefix="/category",tags=["category"])
 
 Dbdependency = Annotated[Session,Depends(get_db)]
 
-@router.get("",response_model=list[CategoryResponse])
+@router.get("",response_model=list[CategoryResponse],status_code=status.HTTP_200_OK)
 async def find_all(db :Dbdependency):
   return category_cruds.find_all(db)
 
-@router.get("/id",response_model=CategoryResponse)
+@router.get("/id",response_model=CategoryResponse,status_code=status.HTTP_200_OK)
 async def find_by_id(id :int,db :Dbdependency):
-  return category_cruds.find_by_id(id,db)
+  found_item = category_cruds.find_by_id(id,db)
+  if not found_item:
+    raise HTTPException(status_code=400,detail=f"カテゴリーID:{id}が見つかりません。")
 
-@router.post("",response_model=CategoryResponse)
+@router.post("",response_model=CategoryResponse,status_code=status.HTTP_201_CREATED)
 async def create(db :Dbdependency,create_category :CategoryCreate):
   return category_cruds.create(db,create_category)
 
-@router.put("",response_model=CategoryResponse)
+@router.put("",response_model=CategoryResponse,status_code=status.HTTP_200_OK)
 async def update(id :int,db :Dbdependency,update_category :CategoryUpdate):
-  return category_cruds.update(id,db,update_category)
+  update_item = category_cruds.update(id,db,update_category)
+  if not update_item:
+    raise HTTPException(status_code=400,detail=f"カテゴリーID:{id}が見つかりません。")
+  return update_item
 
-@router.delete("",response_model=CategoryCreate)
+@router.delete("",response_model=CategoryCreate,status_code=status.HTTP_200_OK)
 async def delete(id :int,db :Dbdependency):
-  return category_cruds.delete(id,db)
+  delete_item = category_cruds.delete(id,db)
+  if not delete_item:
+    raise HTTPException(status_code=400,detail=f"カテゴリーID:{id}が見つかりません。")
+  return delete_item
