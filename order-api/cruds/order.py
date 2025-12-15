23b60e4eaedef92fe08fb6e1405571a43918ca4e
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-12-15 15:14:20 +0900

    feat : create_orderエンドぽインドにトークン認証機能追加
    - APIKeyHeaderを使ってトークンを張り付けて認証
    - ユーザー名、パスワード入力をしない形式

--- order-api/cruds/order.py
+++ order-api/cruds/order.py
@@ -13,10 +13,9 @@ class ProductNotFoundError(Exception):
 class InsufficientStockError(Exception):
     pass
 
-def create_order(db :Session,product_id :int,quantity :int,token :str):
+def create_order(db :Session,product_id :int,quantity :int,user_id :int):
 
-    payload = jwt.decode(token,SECRET_KEY,algorithms=ALGORISM)
-    user_id = payload["id"]
+    
     
     response_product = requests.get(
         f"http://localhost:8001/products/{product_id}"
@@ -42,7 +41,8 @@ def create_order(db :Session,product_id :int,quantity :int,token :str):
         # **order_create.model_dump()
         product_id = product_id,
         quantity = quantity,
-        order_at = datetime.now()
+        order_at = datetime.now(),
+        user_id = user_id
         )
         db.add(new_order)
         db.commit()
@@ -50,7 +50,8 @@ def create_order(db :Session,product_id :int,quantity :int,token :str):
         print(f"New Order ID: {new_order.id}")
         print(f"Product ID: {new_order.product_id}")
         print(f"Quantity: {new_order.quantity}")
-        print(f"Quantity: {new_order.order_at}")
+        print(f"order_at: {new_order.order_at}")
+        print(f"user_id: {new_order.user_id}")
         return new_order
     else:
         raise InsufficientStockError(f"在庫が足りません（在庫：{stock}個、注文数：{quantity}個）")
--- order-api/routers/order.py
+++ order-api/routers/order.py
@@ -5,15 +5,27 @@ from typing import Annotated
 from cruds import order as order_cruds
 from schemas import OrderResponse
 from starlette import status
+from fastapi.security import OAuth2PasswordBearer,APIKeyHeader
+from jose import jwt
 
 router = APIRouter(prefix="/order",tags=["order"])
 
 Dbdependency = Annotated[Session,Depends(get_db)]
+oath2_schema = OAuth2PasswordBearer(tokenUrl="/auth/login")
+SECRET_KEY = "3FIQodO54obEzChoXmZFaprULmWd1KkYqc5GbITvYwA="
+ALGORISM = "HS256"
+api_key_header = APIKeyHeader(name="Authorization")
 
 @router.post("",response_model=OrderResponse,status_code=status.HTTP_201_CREATED)
-async def create_order(db :Dbdependency,product_id :int,quantity :int,token :str):
+async def create_order(db :Dbdependency,product_id :int,quantity :int,authorization: str = Depends(api_key_header)):
+  token = authorization.replace("Bearer ", "")
   try:
-    return order_cruds.create_order(db,product_id,quantity,token)
+    payload = jwt.decode(token,SECRET_KEY,algorithms=ALGORISM)
+    user_id = payload["id"]
+  except:
+    raise HTTPException(status_code=404,detail="不正なトークンです。")
+  try:
+    return order_cruds.create_order(db,product_id,quantity,user_id)
   
   except order_cruds.ProductNotFoundError as e:
     raise HTTPException(status_code=400,detail=str(e))
