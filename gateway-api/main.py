735305a63cafbe8f91e1e6a2b67cdc1e5e2283f2
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-12-17 14:56:56 +0900

    feat : gateway-apiにorderエンドポイント作成
    - order-api/router/order,create_orderエンドポイントを認証しない形へ変更

--- gateway-api/main.py
+++ gateway-api/main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI,Depends,Header,HTTPException
 import requests
 from fastapi.security import OAuth2PasswordBearer
 from typing import Annotated
+from jose import jwt
 
 app = FastAPI()
-FormDependency = Annotated[OAuth2PasswordBearer,Depends()]
+
+oath2_schema = OAuth2PasswordBearer(tokenUrl="/auth/login")
+SECRET_KEY = "3FIQodO54obEzChoXmZFaprULmWd1KkYqc5GbITvYwA="
+ALGORITHM = "HS256"
 
 @app.get("/")
 def top():
@@ -18,10 +22,37 @@ def get_products():
     )
     return response.json()
 
-@app.get("/login")
-def login(username,password):
+@app.post("/login")
+def login(username :str,password :str):
     response = requests.post(
-        "http://localhost:8003/auth/login",
+        "http://localhost:8004/auth/login",
         data={'username':username,'password':password}
     )
+    return response.json()
+
+
+@app.post("/order")
+def create_order(
+    product_id :int,
+    quantity :int,
+    authorization :str = Header(...)
+    ):
+    token = authorization.replace("Bearer ", "")
+    try:
+        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        user_id = payload["user_id"]
+        print(f"User ID: {user_id}")
+    except:
+        raise HTTPException(status_code=401,detail="In valid token")
+    
+    response = requests.post(
+         "http://localhost:8003/order",
+         json={
+             "product_id" :product_id,
+             "quantity" :quantity,
+             "user_id" :user_id
+         }
+    )
+    print(f"Response status: {response.status_code}")  # ← 追加
+    print(f"Response body: {response.text}")  # ← 追加
     return response.json()
--- order-api/routers/order.py
+++ order-api/routers/order.py
@@ -11,22 +11,14 @@ from jose import jwt
 router = APIRouter(prefix="/order",tags=["order"])
 
 Dbdependency = Annotated[Session,Depends(get_db)]
-oath2_schema = OAuth2PasswordBearer(tokenUrl="/auth/login")
-SECRET_KEY = "3FIQodO54obEzChoXmZFaprULmWd1KkYqc5GbITvYwA="
-ALGORISM = "HS256"
+
 api_key_header = APIKeyHeader(name="Authorization")
 
 @router.post("",response_model=OrderResponse,status_code=status.HTTP_201_CREATED)
-async def create_order(db :Dbdependency,product_id :int,quantity :int,authorization: str = Depends(api_key_header)):
-  token = authorization.replace("Bearer ", "")
-  try:
-    payload = jwt.decode(token,SECRET_KEY,algorithms=ALGORISM)
-    user_id = payload["id"]
-  except:
-    raise HTTPException(status_code=404,detail="不正なトークンです。")
+async def create_order(db :Dbdependency,product_id :int,quantity :int,user_id :int):
+  
   try:
     return order_cruds.create_order(db,product_id,quantity,user_id)
-  
   except order_cruds.ProductNotFoundError as e:
     raise HTTPException(status_code=400,detail=str(e))
   except order_cruds.InsufficientStockError as e:
