9551556b152d03c024c17072e0194f6a1d32f713
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-12-20 18:36:35 +0900

    fix : 各関数に型ヒント追加

--- order-api/cruds/order.py
+++ order-api/cruds/order.py
@@ -14,7 +14,7 @@ class InsufficientStockError(Exception):
     pass
 
 
-def fetch_product(product_id):
+def fetch_product(product_id :int):
     response_product = requests.get(
         f"http://localhost:8001/products/{product_id}"
     )
@@ -22,12 +22,12 @@ def fetch_product(product_id):
         raise ProductNotFoundError(f"商品ID:{product_id}が見つかりません。")
     return response_product.json()
 
-def ensure_product_exists(product_id):
+def ensure_product_exists(product_id :int):
     product = fetch_product(product_id)
     return product['id']
 
 
-def fetch_stock(product_id):
+def fetch_stock(product_id :int):
     response_stock = requests.get(
         f"http://localhost:8002/stock/{product_id}"
     )
@@ -38,28 +38,29 @@ def ensure_stock_exists(product_id):
     stock = fetch_stock(product_id)
     return stock['stock']
 
-def confirm_stock(stock,quantity):
+def confirm_stock(stock :int,quantity :int):
     if stock < quantity:
         raise InsufficientStockError(f"在庫が足りません（在庫：{stock}個、注文数：{quantity}個）")
     
 
-def ensure_stock_is_enough(product_id,quantity):
+def ensure_stock_is_enough(product_id :int,quantity :int):
     stock =ensure_stock_exists(product_id)
     confirm_stock(stock,quantity)
+    return stock
 
-def update_stock(product_id,new_stock):
+def update_stock(product_id :int,new_stock :int):
     response = requests.put(
-            f"http://localhost:8002/products/{product_id}",
+            f"http://localhost:8002/stock/{product_id}",
             json={'stock':new_stock}
         )
     
-def update_product_status(product_id):
+def update_product_status(product_id :int):
     response = requests.put(
-            f"http://localhost:8001/stock/{product_id}",
+            f"http://localhost:8001/products/{product_id}",
             json={'status':False}
     )
     
-def create_order_entity(product_id,quantity,user_id):
+def create_order_entity(product_id :int,quantity :int,user_id :int):
     new_order = Order(
         # **order_create.model_dump()
         product_id = product_id,
@@ -69,19 +70,20 @@ def create_order_entity(product_id,quantity,user_id):
         )
     return new_order
 
-def save_order(db,product_id,quantity,user_id):
+def save_order(db :Session,product_id :int,quantity :int,user_id :int):
     new_order = create_order_entity(product_id,quantity,user_id)
     db.add(new_order)
     db.commit()
     db.refresh(new_order)
+    return new_order
 
-def notify_stock(product_id,stock,quantity):
+def notify_stock(product_id :int,stock :int,quantity :int):
     update_stock(product_id,stock-quantity)
 
-def mark_as_unsellable(product_id):
+def mark_as_unsellable(product_id :int):
     update_product_status(product_id)
 
-def notify_product_status(product_id,stock,quantity):
+def notify_product_status(product_id :int,stock :int,quantity :int):
     if stock - quantity ==0:
         mark_as_unsellable(product_id)
 
@@ -91,17 +93,18 @@ OREDER_CONFIRMED_SUBSCRIBERS=[
     notify_product_status
 ]
 
-def publish_order_confirmed(product_id,stock,quantity):
+def publish_order_confirmed(product_id :int,stock :int,quantity :int):
     for handler in OREDER_CONFIRMED_SUBSCRIBERS:
         handler(product_id,stock,quantity)
 
 
 
-def order_confirm(db,product_id,quantity,user_id):
+def order_confirm(db :Session,product_id :int,quantity :int,user_id :int):
     product_id =ensure_product_exists(product_id)
     stock = ensure_stock_is_enough(product_id,quantity)
-    save_order(db,product_id,quantity,user_id)
+    new_order = save_order(db,product_id,quantity,user_id)
     publish_order_confirmed(product_id,stock,quantity)
+    return new_order
     
 
 # def create_order(db :Session,product_id :int,quantity :int,user_id :int):
