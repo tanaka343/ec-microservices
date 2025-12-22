90130aa14fa9c426cc23dc793aae506d19c85f73
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-12-22 11:27:19 +0900

    feat : デフォルトで8004ポートで開く設定を追記

--- auth-api/main.py
+++ auth-api/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from routers import auth
+import uvicorn
 
 app = FastAPI()
 
 app.include_router(auth.router)
+
+
+
+if __name__ == "__main__":
+  uvicorn.run(app,host="127.0.0.1",port=8004)
