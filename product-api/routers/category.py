3da8886a037b202a7298824c9cdf633acc0794e7
tanaka343 <mnhyodo.yc@kaien-t3.net>
2026-01-23 09:17:15 +0900

    fix: find_by_id,update,deleteのパスパラメータを修正

--- product-api/routers/category.py
+++ product-api/routers/category.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter,Depends,HTTPException
 from database import get_db
 from sqlalchemy.orm import Session
-from typing import Annotated
+from typing import Annotated,Optional
 from cruds import ctegory as category_cruds
 from schemas import CategoryResponse,CategoryCreate,CategoryUpdate
 from starlette import status
@@ -14,7 +14,7 @@ Dbdependency = Annotated[Session,Depends(get_db)]
 async def find_all(db :Dbdependency):
   return category_cruds.find_all(db)
 
-@router.get("/id",response_model=CategoryResponse,status_code=status.HTTP_200_OK)
+@router.get("/{id}",response_model=CategoryResponse,status_code=status.HTTP_200_OK)
 async def find_by_id(id :int,db :Dbdependency):
   found_item = category_cruds.find_by_id(id,db)
   if not found_item:
@@ -24,14 +24,14 @@ async def find_by_id(id :int,db :Dbdependency):
 async def create(db :Dbdependency,create_category :CategoryCreate):
   return category_cruds.create(db,create_category)
 
-@router.put("",response_model=CategoryResponse,status_code=status.HTTP_200_OK)
+@router.put("/{id}",response_model=CategoryResponse,status_code=status.HTTP_200_OK)
 async def update(id :int,db :Dbdependency,update_category :CategoryUpdate):
   update_item = category_cruds.update(id,db,update_category)
   if not update_item:
     raise HTTPException(status_code=400,detail=f"カテゴリーID:{id}が見つかりません。")
   return update_item
 
-@router.delete("",response_model=CategoryCreate,status_code=status.HTTP_200_OK)
+@router.delete("/{id}",response_model=Optional[CategoryResponse],status_code=status.HTTP_200_OK)
 async def delete(id :int,db :Dbdependency):
   delete_item = category_cruds.delete(id,db)
   if not delete_item:
